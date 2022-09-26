d288ec0eb402cc49204754feebabafcb74d59d32
Grumpy Cockatiel <tguillory@gmail.com>
2022-09-25 19:42:30 -0500

    page through all email IDs

--- Raydreams.GMailer/Program.cs
+++ Raydreams.GMailer/Program.cs
@@ -4,19 +4,13 @@ using System.Net;
 using System.Net.Mail;
 using System.Text;
 using System.Text.RegularExpressions;
-using Google.Apis.Auth.OAuth2;
-using Google.Apis.Gmail.v1;
-using Google.Apis.Gmail.v1.Data;
-using Google.Apis.Oauth2.v2;
-using Google.Apis.Oauth2.v2.Data;
-using Google.Apis.Services;
 using Microsoft.Extensions.Configuration;
 using MimeKit;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace Raydreams.GMailer
 {
-    /// <summary></summary>
+    /// <summary>Startup Class</summary>
     public static class Program
     {
         static int Main( string[] args )
